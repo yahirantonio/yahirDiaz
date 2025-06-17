b208a2541e2b06629ebdbc176a8801bd975af087
yahirantonio <yahirantoni90@gmail.com>
2025-06-16 20:43:49 -0600

    -feat(header): agregando boton de fondo oscuro o luminoso, agregando el header y sus resoluciones en diferentes pantallas.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon({iconDir: './src/icons'})],
   vite: {
     plugins: [tailwindcss()]
   }
